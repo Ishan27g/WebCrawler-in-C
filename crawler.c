5acdffd127d1f480866bf50c8fc2c54e1da488f1
ishan <igoyal@student-box-295.novalocal>
2020-04-05 06:48:42 +0000

    added parsing of cmdline input

--- crawler.c
+++ crawler.c
@@ -7,16 +7,56 @@
 char* original_host;
 
 
-
+char* parse_input(char* input, char* resource)
+{
+	char *components, *dest_temp;
+	char *host;
+	host = strstr(input,"http://");
+	if(host == NULL)
+	{
+		host = strstr(input,"HTTP://");
+	}
+	if(host == NULL)
+	{
+		//string is in format name.resource
+	}
+	else
+	{
+		//string is in http://name.resource format
+		//char* host not points to name
+		//strcpy(input_string, host+7);
+		components = strtok_r(input+7, "/", &dest_temp);
+		strcpy(host, components);
+		strcpy(resource, components);
+		return host;//components;
+	}
+	return NULL;
+}
 
 int main(int argc, char **argv)
 {	
 	int client_socket;
-	
+	char *resource;
+	char *host;
+	if(argv[1])
+	{
+		host = malloc(strlen(argv[1]));
+		resource = malloc(strlen(argv[1]));
+	}
+	else
+	{
+		printf("\ninvalid input string\n");
+		return NULL;
+	}
+
+	host = parse_input(argv[1], resource);	
+	printf("\nhost is %s", host);
+	printf("\nresource is %s", resource);
+
 	Web_crawler crawler;
-	
+
 	original_host = NULL;
-	
+
 	if(argv[1] == NULL)
 	{
 		printf("\nEnter hostname as 1st arguement\n");
